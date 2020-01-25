83f384273a435507a02b418e323118017095eba8
Gohar2004 <gohar.hakobyan1.y@tumo.org>
2020-01-25 17:43:00 +0400

    aaaaa

---
+++ script.js
@@ -0,0 +1,26 @@
+// var clickCount = 0;
+// function clickHandler(evt){
+//    clickCount++;
+//    console.log(evt);
+//    var str = "Thanks for clicking " + clickCount;
+//    this.innerText = str;
+// }
+
+// var p = document.getElementById("pElement");
+// p.addEventListener("click", clickHandler);
+
+function main() {
+    var socket = io();
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+ 
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
+    button.onclick = handleSubmit;
+ 
+}
